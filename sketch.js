cc39d9aa3cb6cbb3fee88875006f2e593082261b
Verified-Email <71946266+Verified-Email@users.noreply.github.com>
2020-12-10 17:41:55 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    backgroundImg = loadImage("bg.png");
 }
 
 function setup(){
